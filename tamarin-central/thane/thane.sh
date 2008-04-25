4a96ca6c48f866a48ea10766be16f637e3e7fc5c
Par Winzell <zell@threerings.net>
2008-04-25 14:46:01 +0000

    Oops, away with ye, hard-coded path.

--- tamarin-central/thane/thane.sh
+++ tamarin-central/thane/thane.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 #
 
-ASC_DIR=/home/zell/asc
+if [ -z "$1" ]; then
+    echo "Usage: $0 <path to asc.jar>"
+    exit 255
+fi
+
+ASC=$1
 ASC_CLASS=macromedia.asc.embedding.ScriptCompiler
 ASC_OPTS="-strict -abcfuture -AS3 -outdir ."
 
@@ -10,5 +15,5 @@ BUILTIN=../core/builtin.abc
 SOURCES="Thane.as toplevel.as Domain.as StringBuilder.as ByteArray.as Endian.as \
          ../extensions/Dictionary.as thane_core.as thane_net.as Socket.as"
 
-java -ea -DAS3 -DAVMPLUS -DAS3 -classpath $ASC_DIR/lib/asc.jar $ASC_CLASS $ASC_OPTS \
-    -import $BUILTIN -out thane $SOURCES
+java -ea -DAS3 -DAVMPLUS -DAS3 -classpath $ASC $ASC_CLASS $ASC_OPTS  \
+     -import $BUILTIN -out thane $SOURCES
